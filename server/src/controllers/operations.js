12843a7245b5fbd5d7cac653f1853ab60847d4d8
brayanobisto <brayan-28-@hotmail.com>
2022-06-25 05:00:30 -0500

    Add post operations route

--- server/src/controllers/operations.js
+++ server/src/controllers/operations.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator')
+
 const { Operation } = require('../db.js')
 
 module.exports = {
@@ -5,6 +7,27 @@ module.exports = {
     Operation.findAll()
       .then(operations => {
         res.status(200).json(operations)
+      })
+      .catch(next)
+  },
+  createOperation: (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { concept, amount, date, type, category } = req.body
+
+    Operation.create({
+      concept,
+      amount,
+      date,
+      type,
+      category,
+    })
+      .then(operation => {
+        res.status(201).json(operation)
       })
       .catch(next)
   },
--- server/src/routes/operations.js
+++ server/src/routes/operations.js
@@ -1,8 +1,38 @@
 const { Router } = require('express')
 const router = Router()
 
+const { body } = require('express-validator')
+
 const operationsController = require('../controllers/operations')
+const { CATEGORIES } = require('../constants')
 
 router.get('/', operationsController.getAllOperations)
+router.post(
+  '/',
+  body('concept').notEmpty().withMessage('El concepto no puede estar vacío').optional({ nullable: true }),
+  body('amount')
+    .notEmpty()
+    .withMessage('La cantidad es requerida')
+    .isNumeric()
+    .withMessage('La cantidad debe ser un numero')
+    .isFloat({ min: 1 })
+    .withMessage('La cantidad debe ser mayor a 0'),
+  body('date')
+    .notEmpty()
+    .withMessage('La fecha es requerida')
+    .isDate({ delimiters: ['/', '-', ' '] })
+    .withMessage('La fecha debe ser una fecha valida'),
+  body('type')
+    .notEmpty()
+    .withMessage('El tipo es requerido')
+    .isIn(['ingreso', 'egreso'])
+    .withMessage('El tipo debe ser ingreso o egreso'),
+  body('category')
+    .notEmpty()
+    .withMessage('La categoria es requerida')
+    .isIn(CATEGORIES)
+    .withMessage(`La categoría debe ser una de las siguientes: ${CATEGORIES.join(', ')}`),
+  operationsController.createOperation
+)
 
 module.exports = router
