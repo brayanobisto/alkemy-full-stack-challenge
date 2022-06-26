2c3ec1df8cf0887fe4a44184375ce5fc3afa601d
brayanobisto <brayan-28-@hotmail.com>
2022-06-26 13:33:48 -0500

    Add Operations page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home, Login, Register } from '@pages'
+import { Home, Operations, Login, Register } from '@pages'
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/operaciones" element={<Operations />} />
       <Route path="/registro" element={<Register />} />
       <Route path="/ingreso" element={<Login />} />
     </Routes>
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './Home'
+export * from './Operations'
 export * from './Register'
 export * from './Login'
