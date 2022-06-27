327b7a8d3f48f4e25a7a297faab4638b3d1398e4
brayanobisto <brayan-28-@hotmail.com>
2022-06-27 00:37:16 -0500

    Add form validation & request

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,44 +1,89 @@
 import type { FC } from 'react'
 
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { useAuth } from '@hooks'
+import { authSchema } from '@schemas'
+
+export interface IUserForm {
+  email: string
+  password: string
+  confirmPassword: string
+}
 
 export const Register: FC = () => {
+  const { register } = useAuth()
+
+  const {
+    register: registerInput,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<IUserForm>({
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    resolver: yupResolver(authSchema.register),
+  })
+
+  const onSubmit = ({ email, password }: IUserForm) =>
+    register(email, password).catch(({ response }) => {
+      console.log(response)
+      const [error, ...errors] = response.data.errors
+      setError(error.param, { type: 'register', message: error.msg }, { shouldFocus: true })
+
+      // TODO:Add errors types
+      errors !== undefined &&
+        errors.forEach((error: any) => {
+          setError(error.param, { type: 'register', message: error.msg })
+        })
+    })
+
   return (
     <div className="flex flex-col justify-center min-h-screen p-8">
       <h1 className="mb-4 text-2xl font-bold text-center">Crear cuenta</h1>
 
-      <form className="self-center w-full max-w-sm p-6 mb-2 rounded-md shadow-md bg-slate-200">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="self-center w-full max-w-sm p-6 mb-2 rounded-md shadow-md bg-slate-200"
+      >
         <div className="flex flex-col mb-4">
           <input
             autoComplete="off"
             type="email"
-            id="email"
+            {...registerInput('email')}
             className="px-4 py-2 border border-gray-300 rounded-md outline-none"
             placeholder="Email"
           />
-          <span className="mt-1 text-rose-600">El email es requerido</span>
+          {errors.email && <span className="mt-1 text-xs text-justify text-rose-600">{errors.email.message}</span>}
         </div>
 
         <div className="flex flex-col mb-4">
           <input
             autoComplete="off"
             type="password"
-            id="password"
+            {...registerInput('password')}
             className="px-4 py-2 border border-gray-300 rounded-md outline-none"
             placeholder="Contraseña"
           />
-          <span className="mt-1 text-rose-600">La contraseña es requerida</span>
+          {errors.password && <span className="mt-1 text-xs text-justify text-rose-600">{errors.password.message}</span>}
         </div>
 
         <div className="flex flex-col mb-4">
           <input
             autoComplete="off"
             type="password"
-            id="confirmPassword"
+            {...registerInput('confirmPassword')}
             className="px-4 py-2 border border-gray-300 rounded-md outline-none"
             placeholder="Confirmar contraseña"
           />
-          <span className="mt-1 text-rose-600">La contraseña es requerida</span>
+          {errors.confirmPassword && (
+            <span className="mt-1 text-xs text-justify text-rose-600">{errors.confirmPassword.message}</span>
+          )}
         </div>
 
         <button
