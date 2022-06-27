606c4d0df34096432642f38223061e40018ba9d6
brayanobisto <brayan-28-@hotmail.com>
2022-06-27 00:24:46 -0500

    Add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 
+const FRONTEND_URL = process.env.FRONTEND_URL
+
 // Middlewares
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', FRONTEND_URL) // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 app.use(express.json())
 app.use('/', routes)
 
