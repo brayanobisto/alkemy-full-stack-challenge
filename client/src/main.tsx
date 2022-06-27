ca586199104b2fc60136a10090f2098370009e50
brayanobisto <brayan-28-@hotmail.com>
2022-06-27 00:35:30 -0500

    Add axios

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,18 @@ import App from './App'
 import './index.css'
 
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+
+import { AuthProvider } from '@contexts'
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
