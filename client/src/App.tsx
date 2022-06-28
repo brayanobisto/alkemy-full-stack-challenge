fa978570911523cbcb4bbde9a496bfd87e19b28a
brayanobisto <brayan-28-@hotmail.com>
2022-06-28 17:54:20 -0500

    Add react-hot-toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 import { Home, Operations, Login, Register } from '@pages'
 
 function App(): JSX.Element {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/operaciones" element={<Operations />} />
-      <Route path="/registro" element={<Register />} />
-      <Route path="/ingreso" element={<Login />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/operaciones" element={<Operations />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/ingreso" element={<Login />} />
+      </Routes>
+
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 5000,
+        }}
+      />
+    </>
   )
 }
 
